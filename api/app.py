911701dc521d45d8053e5168adec72039e4537f4
Shubham Attri <shubham21354@iiitd.ac.in>
2024-05-07 12:17:38 +0530

    updated app.py

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 import psutil
 import logging
 import os
+import tasks
 
 app = Flask(__name__)
 CORS(app)
@@ -12,23 +13,51 @@ logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s 
 
 @app.route('/')
 def home():
+    """
+    This function returns a string indicating that the Flask app is running.
+    """
     return "Flask app is running"
 
 @app.route('/generate-text', methods=['POST'])
 def generate_text():
+    """
+    Generate text based on the provided prompt.
+
+    Returns:
+        A JSON response containing the generated text.
+    """
     prompt = request.json.get('prompt')
+    
     if not prompt:
         return jsonify({'error': 'No prompt provided'}), 400
 
     # Log RAM consumption
     ram_usage = psutil.Process(os.getpid()).memory_info().rss
-    logging.info(f"RAM usage for generate-text request: {ram_usage} bytes")
+    # The `rss` attribute of `memory_info()` returns the Resident Set Size (RSS),
+    # which represents the amount of memory currently occupied by the process.
+    # This can be used to monitor the RAM consumption of the application.
+
+    # Rest of the code...
+    logging.info(f"RAM usage for generate-text request: {ram_usage} Mb")
 
     generated_text = tasks.generate_text(prompt)
     return jsonify({'generated_text': generated_text})
 
 @app.route('/summarize-text', methods=['POST'])
 def summarize_text():
+    """
+    Summarizes the provided text.
+
+    Retrieves the text from the request JSON payload and checks if it exists.
+    If the text is not provided, it returns an error response.
+    Logs the RAM consumption for the summarize-text request.
+    Calls the `summarize_text` function from the `tasks` module to generate the summary.
+    Returns the summary as a JSON response.
+
+    Returns:
+        A JSON response containing the summary.
+
+    """
     text = request.json.get('text')
     if not text:
         return jsonify({'error': 'No text provided'}), 400
@@ -42,6 +71,17 @@ def summarize_text():
 
 @app.route('/answer-question', methods=['POST'])
 def answer_question():
+   
+    """
+    Answer a question based on the provided question and context.
+
+    Returns:
+        A JSON response containing the answer to the question.
+
+    Raises:
+        - 400 Bad Request if no question or context is provided.
+    """
+    
     question = request.json.get('question')
     context = request.json.get('context')
     if not question:
